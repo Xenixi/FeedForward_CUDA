6e19c1bf37ff44c3305c52acd38d8a741635d040
Kobe McManus <kobe.mcmanus@xenixi.com>
2021-01-11 22:44:59 -0500

    implemented rough, rest of the train function for GPU kernel

--- main/NetworkBackbone.cu
+++ main/NetworkBackbone.cu
@@ -17,26 +17,39 @@
     }
 }
 */
-__global__ void trainNetwork(float *inputs, float *targets, float *weightsInputHidden, float *weightsHiddenOutput, int iNodes, int hNodes, int oNodes)
+__global__ void trainNetwork(float *inputs, float *targets, float *weightsInputHidden, float *weightsHiddenOutput, int iNodes, int hNodes, int oNodes, float *learningRate, float *outputErrs, float *finalOutputs, float *hiddenOutputs, float *hiddenErrs)
 {
     //  THE INPUT VARIABLE PASSED IN WILL BE REPLACED WITH THE RETURNED ERROR VALUES SO MAKE A COPY OF THE VARIABLE!!!!
+    
+    //does this work in CUDA GPU kernel function?...
+    float *inputsOrig = inputs;
+
+
+
     int idx = blockDim.x * blockIdx.x + threadIdx.x, stride = blockDim.x * gridDim.x;
 
     for (int i = idx; i < iNodes; i += stride)
     {
         inputs[i] = inputs[i] * weightsInputHidden[i];
-        inputs[i] = activation(inputs[i]);
-        inputs[i] = inputs[i] * weightsHiddenOutput[i];
-        inputs[i] = activation(inputs[i]);
-        inputs[i] = weightsHiddenOutput[i] * inputs[i];
-        inputs[i] = activation(inputs[i]);
+        hiddenOutputs[i] = activation(inputs[i]);
 
-        inputs[i] = targets[i] - inputs[i];
+    /// final_inputs = numpy.dot(self.who, hidden_outputs)
+        inputs[i] = weightsHiddenOutput[i] * hiddenOutputs[i];
+    /// final_outputs = self.activation_function(final_inputs)
+        finalOutputs[i] = activation(inputs[i]);
+    ///output_errors = targets - final_outputs
+        outputErrs[i] = targets[i] - finalOutputs[i];
+    ///hidden_errors = numpy.dot(self.who.T, output_errors)
+        hiddenErrs[i] = weightsHiddenOutput[i] * outputErrs[i];
+    //// self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+    //// self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+    //**************************************************************************
+    //  IF SOMETHING GOES WRONG IT'S PROBABLY THIS THIS IS PROBABLY BROKEN MAY NEED TO BE FIXED- THE PY ONE USED NUMPY TRANSPOSE AND 2D ARRAYS THIS DOESN'T
+       weightsHiddenOutput[i] += (learningRate * (outputErrs[i]*finalOutputs[i]*(1.0-finalOutputs[i])) * hiddenOutputs[i]);
+       weightsInputHidden[i] += (learningRate * (hiddenErrs[i]*hiddenOutputs[i]*(1.0-hiddenOutputs[i])) * inputsOrig[i]);
+
         ///LINE 66 LEFT OFF (PYTHON PROGRAM)
-
         ///NUMPY.TRANSPOSE LINES AND STUFF FROM THE PYTHON (LINE 71 IN THE OTHER PROGRAM - )
-
-
     }
 }
 
@@ -126,18 +139,26 @@ void NetworkBackbone::init(float *weightsInputHidden, float *weightsHiddenOutput
     }
 }
 
-void NetworkBackbone::train(float *inputs, float *targets)
+void NetworkBackbone::train(float *inputs, float *targets, float *learningRate, float *outputErrs, float *finalOutputs, float *hiddenOutputs, float *hiddenErrs)
 {
     //allocate shared memory GPU
     //Maybe move ALL values to GPU ahead of time?
     cudaMallocManaged(&inputs, sizeof(float) * getInputQuantity());
     cudaMallocManaged(&targets, sizeof(float) * getOutputQuantity());
 
+    //only need 1 for the learning rate (not an array-like type)
+    cudaMallocManaged(&learningRate, sizeof(float));
+
+    cudaMallocManaged(&outputErrs, sizeof(float) * getOutputQuantity());
+    cudaMallocManaged(&finalOutputs, sizeof(float) * getOutputQuantity());
+    cudaMallocManaged(&hiddenOutputs, sizeof(float) * getHiddenQuantity());
+    cudaMallocManaged(&hiddenErrs, sizeof(float) * getHiddenQuantity());
+
     //kernel call
     int blocks = (getDeviceProperties()[0] * getDeviceProperties()[2] / getDeviceProperties()[1]);
     int blockThreads = getDeviceProperties()[1];
 
-    trainNetwork<<<blocks, blockThreads>>>(inputs, targets, getInputQuantity(), getHiddenQuantity(), getOutputQuantity());
+    trainNetwork<<<blocks, blockThreads>>>(inputs, targets, getInputQuantity(), getHiddenQuantity(), getOutputQuantity(), learningRate, outputErrs, finalOutputs, hiddenOutputs, hiddenErrs);
 
     //free
 
